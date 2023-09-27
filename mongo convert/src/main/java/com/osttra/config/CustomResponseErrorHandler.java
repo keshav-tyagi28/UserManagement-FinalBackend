9e4b00aeeef9c6c70abded29765017d85d71da97
rishika-ihsmarkit <rishika.singh@ihsmarkit.com>
2023-09-27 12:53:04 +0530

    logger added

--- mongo convert/src/main/java/com/osttra/config/CustomResponseErrorHandler.java
+++ mongo convert/src/main/java/com/osttra/config/CustomResponseErrorHandler.java
@@ -2,6 +2,7 @@ package com.osttra.config;
 
 
 import org.springframework.context.annotation.Configuration;
+import java.util.logging.Logger;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpResponse;
@@ -13,15 +14,17 @@ import java.io.IOException;
 
 @Configuration
 public class CustomResponseErrorHandler extends DefaultResponseErrorHandler {
+	
+    private static final Logger LOGGER = Logger.getLogger(CustomResponseErrorHandler.class.getName());
 
     @Override
     public void handleError(ClientHttpResponse response) throws IOException {
         if (  response.getStatusCode() == HttpStatus.NO_CONTENT) {
             // Log the 204 No Content response as successful, but don't throw an exception
-            System.out.println("Received a 204 No Content response. It's treated as successful.");
+            LOGGER.info("Received a 204 No Content response. It's treated as successful.");
         } else {
             // For other response codes, you can handle them as needed or throw exceptions
-        	System.out.println("hello");
+            LOGGER.warning("Received an error response with status code: " + response.getStatusCode());
             super.handleError(response);
         }
     }
